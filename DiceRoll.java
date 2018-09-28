b83ee1218aec611eb5799cc48f67541cb4fa676c
Greg Westmoreland <gwestmor@us.ibm.com>
2018-09-28 16:32:57 -0500

    Add Compare Numbers Using Boolean and Dice Programs

---
+++ DiceRoll.java
@@ -0,0 +1,20 @@
+package diceroll;
+
+/**
+ * This program simulates tosses of a pair of dice.
+ * @author gwestmoreland
+ */
+public class DiceRoll {
+
+    public static void main(String[] args) 
+    {
+        for (int i = 1; i <= 10; i++) 
+        {
+            // Generate two random numbers between 1 and 6
+            int d1 = (int) (Math.random() * 6) + 1;
+            int d2 = (int) (Math.random() * 6) + 1;
+            System.out.println(d1 + " " + d2);
+        }
+        System.out.println();
+    }
+}
