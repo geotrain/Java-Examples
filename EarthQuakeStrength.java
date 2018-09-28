53f15e97cb5f1a44eefa92a07957b304601f9cba
Greg Westmoreland <gwestmor@us.ibm.com>
2018-09-28 16:38:03 -0500

    Add Do Log Square Root Output and Earth Quake Strength

---
+++ EarthQuakeStrength.java
@@ -0,0 +1,35 @@
+package earthquakestrength;
+import java.util.Scanner;
+/**
+ * This program gives a description of an earthquake, given the Richter scale magnitude.
+ * @author gwestmoreland
+ */
+public class EarthquakeStrength {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number on the Richter Scale: ");
+        double richter = in.nextDouble(); 
+        
+        //Print the description
+        if (richter >= 8.0)
+        {
+            System.out.println("Most structures fall");
+        }
+        else if (richter >= 7.0)
+        {
+            System.out.println("Many buildings destroyed");
+        }
+        else if (richter >= 6.0)
+        {
+            System.out.println("Many buildings considerably damaged, some collapse");
+        }
+        else if (richter >= 4.5)
+        {
+            System.out.println("Damage to poorly constructed buildings");
+        }
+        else {
+            System.out.println("No destruction of buildings"); 
+        }
+    }
+}
