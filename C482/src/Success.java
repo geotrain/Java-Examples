e432d36d2acfa5bec8ef9cb15fb48338123b37e7
Greg Westmoreland <gwestmor@us.ibm.com>
2020-08-08 00:09:21 -0500

    more files added

---
+++ C482/src/Success.java
@@ -0,0 +1,12 @@
+public class Success {
+    public static void main(String[] args) {
+        // Today's Date is August 8, 2020
+        System.out.println("Greg Westmoreland");
+        System.out.println("3303 Athens Drive");
+        System.out.print("Corinth, ");
+        System.out.print("TX ");
+        System.out.print("76210\n");
+        System.out.println("214-499-0833");
+        System.out.println("Enter a value that is greater than zero and less than 10." );
+    }
+}
---
+++ C482/src/Wolfgang.java
@@ -0,0 +1,14 @@
+// The Works of Wolfgang
+public class Wolfgang
+{
+    public static void main(String[] args)
+    {
+        System.out.print("The works of Wolfgang " +
+                "include ");
+                System.out.print("the following");
+        System.out.print(" " +
+                "The Turkish March ");
+                System.out.print("and Symphony No. 40 ");
+        System.out.println("in G minor.");
+    }
+}
