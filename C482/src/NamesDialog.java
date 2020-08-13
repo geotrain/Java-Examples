3ac292bd3e1dbd2335b1647261b16477f8db1c61
Greg Westmoreland <gwestmor@us.ibm.com>
2020-08-12 21:37:18 -0500

    newly added files

---
+++ C482/src/NamesDialog.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+/**
+ This program demonstrates using dialogs
+ with JOptionPane.
+ */
+
+public class NamesDialog {
+    public static void main(String[] args) {
+        String firstName, lastName;
+        System.exit(0);
+    }
+}
